1e1a55899ba54f0cf9040de71c6e76e3392577f2
Itsmejeff <156865317+Itsmejeff@users.noreply.github.com>
2024-02-15 19:10:11 -1200

    Add files via upload

---
+++ sc.js
@@ -0,0 +1,34 @@
+var screen = document.querySelector('#screen');
+ var btn = document.querySelectorAll('.btn');
+
+ /*============ For getting the value of btn, Here we use for loop ============*/
+ for (item of btn) {
+     item.addEventListener('click', (e) => {
+         btntext = e.target.innerText;
+
+         if (btntext == '×') {
+             btntext = '*';
+         }
+
+         if (btntext == '÷') {
+             btntext = '/';
+         }
+         screen.value += btntext;
+     });
+ }
+ function fact() {
+     var i, num, f;
+     f = 1
+     num = screen.value;
+     for (i = 1; i <= num; i++) {
+         f = f * i;
+     }
+
+     i = i - 1;
+
+     screen.value = f;
+ }
+
+ function backspc() {
+     screen.value = screen.value.substr(0, screen.value.length - 1);
+ }
